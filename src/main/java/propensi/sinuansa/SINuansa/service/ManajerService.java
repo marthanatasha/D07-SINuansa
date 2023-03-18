2e23d952462ad8f70b4a4676d551441458d62d87
putri.martha <putri.martha@ui.ac.id>
2023-03-18 21:48:32 +0700

    Fitur menambahkan akun (backend)

--- src/main/java/propensi/sinuansa/SINuansa/service/ManajerService.java
+++ src/main/java/propensi/sinuansa/SINuansa/service/ManajerService.java
@@ -4,5 +4,6 @@ import propensi.sinuansa.SINuansa.model.Manajer;
 
 public interface ManajerService {
     Manajer findManajerId(Long Id);
+    void addManajer(Manajer manajer);
 
 }
--- src/main/java/propensi/sinuansa/SINuansa/service/UserService.java
+++ src/main/java/propensi/sinuansa/SINuansa/service/UserService.java
@@ -1,8 +1,10 @@
 package propensi.sinuansa.SINuansa.service;
 
 import propensi.sinuansa.SINuansa.model.User;
+import java.util.List;
 
 public interface UserService {
     User findUserId(Long Id);
-
+    List<User> getListUser();
+    void addUser(User user);
 }
