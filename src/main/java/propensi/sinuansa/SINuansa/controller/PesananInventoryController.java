9ea168007664886e4ab32cad6c84416598ebd5de
Michael Wong <michael.wong@ui.ac.id>
2023-03-20 22:14:26 +0700

    Finishing PBI-10 (Melihat Pesanan Inventori)

--- src/main/java/propensi/sinuansa/SINuansa/controller/PesananInventoryController.java
+++ src/main/java/propensi/sinuansa/SINuansa/controller/PesananInventoryController.java
@@ -2,7 +2,6 @@ package propensi.sinuansa.SINuansa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
