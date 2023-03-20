7e62d5df89e727a1b6cbab237ab518656bd4f4dd
Michael Wong <michael.wong@ui.ac.id>
2023-03-20 20:56:13 +0700

    Sprint 1/PBI-9b/Membuat Pesanan Inventory (not done) PBI-9c/Melihat Pesanan Inventory (half done)

--- src/main/java/propensi/sinuansa/SINuansa/service/PesananInventoryService.java
+++ src/main/java/propensi/sinuansa/SINuansa/service/PesananInventoryService.java
@@ -2,7 +2,10 @@ package propensi.sinuansa.SINuansa.service;
 
 import propensi.sinuansa.SINuansa.model.PesananInventory;
 
+import java.util.List;
+
 public interface PesananInventoryService {
     PesananInventory findPesananInventoryId(Long Id);
-
+    public PesananInventory addPesananInventory(PesananInventory pesananInventory);
+    public List<PesananInventory> getListPesananInventory();
 }
